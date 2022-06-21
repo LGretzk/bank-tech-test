e3e3b113540a98994906d8e7d8644334fc6f81ce
LGretzk <LGretzk@protonmail.com>
2022-06-21 20:34:24 +0100

    Could not mock the TransactionHistory

--- bankAccount.spec.js
+++ bankAccount.spec.js
@@ -3,28 +3,49 @@ const TransactionHistory = require('./transactionHistory');
 const Transaction = require('./transaction');
 const PrintStatement = require('./printStatement');
 
-describe("BankAccount", () => {
+// jest.mock('./transactionHistory', () => {
+//   return jest.fn().mockImplementation(() => {
+//     return {
+//       init: jest.fn(() => {}),
+//       add: jest.fn(() => {})
+//     };
+//   });
+// });
+jest.mock('./transactionHistory');
+jest.mock('./transaction');
+jest.mock('./printStatement');
 
-  it("initiates an instance of BankAccount", () => {
+describe("BankAccount", () => {
+  // beforeEach(() => {
+  //   mockTransaction = new Transaction();
+  //   mockTransactionHistory = new TransactionHistory();
+  //   mockPrintStatement = new PrintStatement();
+  //   testBankAccount = new BankAccount(mockTransactionHistory, mockTransaction, mockPrintStatement);
+  // })
+
+  xit("initiates an instance of BankAccount", () => {
     testBankAccount = new BankAccount(TransactionHistory, Transaction, PrintStatement);
     expect(testBankAccount).toBeInstanceOf(BankAccount);
   });
   
-  describe("deposit", () => {
-    it("accepts 50 and increases balance by 50", () => {
-      testBankAccount = new BankAccount(TransactionHistory, Transaction, PrintStatement);
-      testBankAccount.deposit(50);
-      expect(testBankAccount.getBalance()).toBe(50);
-    });
+  it("deposit accepts 50 and increases balance by 50", () => {
+    const mockTransaction = new Transaction();
+    const mockPrintStatement = new PrintStatement();
+    const mockTransactionHistory = new TransactionHistory();
+    const testBankAccount = new BankAccount(mockTransactionHistory, mockTransaction, mockPrintStatement);
+    mockTransactionHistory.add = jest.fn();
+    testBankAccount.deposit(50);
+
+    expect(testBankAccount.getBalance()).toBe(50);
+    expect(mockTransactionHistory.add).toHaveBeenCalled();
   });
 
-  describe("withdrawal", () => {
-    it("accepts 40 and decreases balance by 40", () => {
-      testBankAccount = new BankAccount(TransactionHistory, Transaction, PrintStatement);
-      testBankAccount.deposit(50);
-      testBankAccount.withdraw(40);
-      expect(testBankAccount.getBalance()).toBe(10);
-    });
+
+  it("accepts 40 and decreases balance by 40", () => {
+    testBankAccount = new BankAccount(TransactionHistory, Transaction, PrintStatement);
+    testBankAccount.deposit(50);
+    testBankAccount.withdraw(40);
+    expect(testBankAccount.getBalance()).toBe(10);
   });
 
   describe("printStatement", () => {
